b86a66f96ccd06619932c9e7256c8b3f61969cc9
Anthea Alberto <antheajeanne.alberto@unibas.ch>
2026-01-13 14:26:21 +0100

    Update bad_script.R

--- crash-course/clean/cc_exercise/bad_script.R
+++ crash-course/clean/cc_exercise/bad_script.R
@@ -2,6 +2,8 @@ library(ggplot2)
 # Go here: https://www.parlgov.org/data/parlgov-development_csv-utf-8/view_election.csv 
 # Right click and save page as "view_election.csv"
 
+setwd("/Users/albert0003/Documents/GitHub/clean-code/course/clean_code_in_context/cc_exercise")
+
 df <- read.csv("view_election.csv")
 
 names(df) 
@@ -14,25 +16,36 @@ range(df$election_date)
 
 x <- df[df$election_date >= "2019-01-01",] # nur die letzte Wahl
 
-ggplot(x[x$seats>1,], aes(x=party_name_short, y=seats, fill=party_name_short)) + geom_col() +
+plot_final <- 
+  ggplot(x[x$seats>1,], aes(x=party_name_short, y=seats, fill=party_name_short)) + geom_col() +
   labs(x="",
        y="Sitze im Parlament",
        title="Nationalratswahl 2019")
+plot_final
 
-ggplot(x[x$seats>1,], aes(x=party_name_short, y=seats, fill=party_name_short)) + geom_col(show.legend = F) + theme_minimal() +
+
+plot_final <- 
+  ggplot(x[x$seats>1,], aes(x=party_name_short, y=seats, fill=party_name_short)) + geom_col(show.legend = F) + theme_minimal() +
   labs(x="",
        y="Sitze im Parlament",
        title="Nationalratswahl 2019")
+plot_final
 
-ggplot(x[x$seats>1,], aes(x=party_name_short, y=seats, fill=party_name_short)) + geom_col(show.legend = F) + theme_minimal() +
+plot_final <- 
+  ggplot(x[x$seats>1,], aes(x=party_name_short, y=seats, fill=party_name_short)) + geom_col(show.legend = F) + theme_minimal() +
   scale_fill_manual(values = c("yellow","khaki1","royalblue","darkseagreen","chartreuse","orange","red","green4")) + 
   labs(x="",
        y="Sitze im Parlament",
        title="Nationalratswahl 2019")
+plot_final
 
 
-ggplot(x[x$seats>1,], aes(x=reorder(party_name_short, -seats), y=seats, fill=party_name_short)) + geom_col(show.legend = F) + theme_minimal() +
+plot_final <- 
+  ggplot(x[x$seats>1,], aes(x=reorder(party_name_short, -seats), y=seats, fill=party_name_short)) + geom_col(show.legend = F) + theme_minimal() +
   scale_fill_manual(values = c("yellow","khaki1","royalblue","darkseagreen","chartreuse","orange","red","green4")) + 
   labs(x="",
        y="Sitze im Parlament",
        title="Nationalratswahl 2019")
+plot_final
+
+# save this somewhere
